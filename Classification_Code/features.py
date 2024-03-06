850e1041c7d5b68efa184aabcf2e5f755b5fa884
Sebastian Mai <sebastian.mai@uni-konstanz.de>
2024-03-06 11:52:20 +0100

    repair features.py

--- Classification_Code/features.py
+++ Classification_Code/features.py
@@ -6,39 +6,26 @@ import tsfel
 
 def features(data, cybres):
     means = []
-    for i in range(len(data)):
+    for i in range(0,1):
         if cybres:
             data_ch1 = data[i][['timestamp', 'differential_potential_CH1']]
             data_ch2 = data[i][['timestamp', 'differential_potential_CH1']]
 
-            data_ch1.drop(['timestamp'])
-            data_ch2.drop(['timestamp'])
-
             intervals = []
             for start in [0, 10, 20]:
-                interval = get_first_min_interval(data_ch1, start, start + 10)
+                interval = get_first_min_interval(data_ch1, start, 10)
                 intervals.append(interval)
-
+            print(type(intervals[0]))
             ch1_mean_1 = tsfel.calc_mean(intervals[0])
             ch1_mean_2 = tsfel.calc_mean(intervals[1])
-
-            ch2_mean = np.mean(data_ch2)
-            ch1_variance = np.var(data_ch1)
-            ch2_variance = np.var(data_ch2)
-            ch1_skweness = stats.skew(data_ch1)
-            ch2_skewness = stats.skew(data_ch2)
-            ch1_kurtosis = stats.kurtosis(data_ch1)
-            ch2_kurtosis = stats.kurtosis(data_ch2)
-            means.append((ch1_mean, ch2_mean, ch1_variance, ch2_variance, ch1_skweness, ch2_skewness, ch1_kurtosis,
-                          ch2_kurtosis))
-
+            ch2_mean_3 = tsfel.calc_mean(intervals[2])
     return means
 
 def get_first_min_interval(data, start, ending):
     date = data['timestamp'].iloc[0].replace(second=0, microsecond=0)
     start = date + pd.Timedelta(minutes=start)
     end = start + pd.Timedelta(minutes=ending)
-    interval = data[(data['timestamp'] >= start) & (data['timestamp'] < end)].copy()
+    interval = data[(data['timestamp'] >= start) & (data['timestamp'] < end)].copy().drop(columns=['timestamp'])
 
     return interval
 
